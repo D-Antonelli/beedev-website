89aaa4d5e30378efa565e703a908eef2a1cb69c0
Derya Antonelli <derya.antonelli@gmail.com>
2021-08-04 17:09:35 +0100

    Add hero section

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,34 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import Header from '../components/header/header';
+import Header from '../components/header';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Beedev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to our demo blog!</h1>
-        <p>
-          You can find more articles on the{' '}
-          <Link href="/blog">
-            <a>blog articles page</a>
-          </Link>
-        </p>
+        <section className="hero">
+          <div className={styles.heroContainer}>
+            <div className={styles.heroLeft}>
+              <h1 className={styles.mainTitle}>welcome to the blogging website</h1>
+              <p className={styles.secondaryTitle}>
+                this website is powered by next js on the frontend, wordpress
+                cms in the backend.
+              </p>
+              <Link href="/blog" passHref>
+                <button className={styles.cta}>read all blogs</button>
+              </Link>
+            </div>
+          </div>
+        </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
