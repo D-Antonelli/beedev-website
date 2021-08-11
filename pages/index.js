7ddb1b43031aee1da8a378c150407de24415e755
Derya Antonelli <derya.antonelli@gmail.com>
2021-08-11 14:41:45 +0100

    Refactor index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import Header from '../components/header';
-import Main from '../components/main';
+import Article from '../components/article';
+import Hero from '../components/hero';
+import ArticleGrid from '../components/article-grid';
 import styles from '../styles/Home.module.css';
+import { getRecentPosts } from '../lib/api';
+import Articles from '../components/articles';
 
-export default function Home() {
+const Home = ({ recentPosts }) => {
+  const posts = recentPosts.edges;
   return (
     <div className={styles.container}>
       <Head>
@@ -13,8 +16,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Main/>
+      <main>
+        <Hero />
+        <Articles>
+          <ArticleGrid>
+            {posts.map(({ node }) => {
+              <Article
+                key={node.id}
+                src={node.featuredImage.node.mediaItemUrl}
+                alt={node.featuredImage.node.altText}
+                title={node.title}            
+              />;
+            })}
+          </ArticleGrid>
+        </Articles>
+      </main>
       <footer className={styles.footer}></footer>
     </div>
   );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  const recentPosts = await getRecentPosts();
+  return {
+    props: {
+      recentPosts,
+    },
+  };
 }
