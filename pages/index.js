d3acfba4f6a22306ff3a914b5403648458858958
Derya Antonelli <derya.antonelli@gmail.com>
2021-08-12 10:42:04 +0100

    Refactor component name for readability

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head';
 import Header from '../components/header';
-import Article from '../components/article';
 import Hero from '../components/hero';
 import ArticleGrid from '../components/article-grid';
 import styles from '../styles/Home.module.css';
 import { getRecentPosts } from '../lib/api';
-import Articles from '../components/articles';
+import LatestArticles from '../components/articles';
 
 const Home = ({ recentPosts }) => {
   const posts = recentPosts.edges;
@@ -18,18 +17,9 @@ const Home = ({ recentPosts }) => {
       <Header />
       <main>
         <Hero />
-        <Articles>
-          <ArticleGrid>
-            {posts.map(({ node }) => {
-              <Article
-                key={node.id}
-                src={node.featuredImage.node.mediaItemUrl}
-                alt={node.featuredImage.node.altText}
-                title={node.title}            
-              />;
-            })}
-          </ArticleGrid>
-        </Articles>
+        <LatestArticles>
+          <ArticleGrid posts={posts}/>
+        </LatestArticles>
       </main>
       <footer className={styles.footer}></footer>
     </div>
