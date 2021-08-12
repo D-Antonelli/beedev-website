21bc86e62e16e40424637e01601167c3f1ccc525
Derya Antonelli <derya.antonelli@gmail.com>
2021-08-12 10:42:34 +0100

    Place child component to the parent

--- components/article-grid.js
+++ components/article-grid.js
@@ -1,10 +1,17 @@
 import styles from '../styles/Articles.module.css';
+import Article from './article';
 
-const ArticleGrid = ({children}) => (
+const ArticleGrid = ({ posts }) => (
   <div className={styles.articleGrid}>
-    {children}
+    {posts.map(({ node }) => {
+      <Article
+        key={node.id}
+        src={node.featuredImage.node.mediaItemUrl}
+        alt={node.featuredImage.node.altText}
+        title={node.title}
+      />;
+    })}
   </div>
 );
 
 export default ArticleGrid;
-
