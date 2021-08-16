43c0ea14526ce165d9be84c4544d002bb120193c
Derya Antonelli <derya.antonelli@gmail.com>
2021-08-16 16:30:46 +0100

    Bug fix: Add return statement to map function

--- components/article-grid.js
+++ components/article-grid.js
@@ -1,17 +1,20 @@
-import styles from '../styles/Articles.module.css';
+import styles from '../styles/ArticleGrid.module.css';
 import Article from './article';
 
-const ArticleGrid = ({ posts }) => (
-  <div className={styles.articleGrid}>
-    {posts.map(({ node }) => {
-      <Article
-        key={node.id}
-        src={node.featuredImage.node.mediaItemUrl}
-        alt={node.featuredImage.node.altText}
-        title={node.title}
-      />;
-    })}
-  </div>
-);
+const ArticleGrid = ({ posts }) => {
+  return (
+    <div className={styles.articleGrid}>
+      {posts.map(({ node }) =>
+          <Article
+            key={node.id}
+            src={node.featuredImage.node?.mediaItemUrl}
+            slug={node.slug}
+            alt={node.featuredImage.node?.altText}
+            title={node.title}
+          />
+      )}
+    </div>
+  );
+};
 
 export default ArticleGrid;
