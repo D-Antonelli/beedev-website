0d9d04949ba9266f7f6860a67b10e33e5ad19ba8
Derya Antonelli <derya.antonelli@gmail.com>
2021-08-28 21:48:09 +0100

    ✅ add:  props type

--- components/flex-container.tsx
+++ components/flex-container.tsx
@@ -1,6 +1,7 @@
-import styles from "../styles/FlexContainer.module.css";
+import React from 'react';
+import styles from '../styles/FlexContainer.module.css';
 
-const FlexContainer = ({ children }) => (
+const FlexContainer = ({ children }: { children: React.ReactNode }) => (
   <div className={styles.flexContainer}>{children}</div>
 );
 
